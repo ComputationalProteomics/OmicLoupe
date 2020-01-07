6eabb521107a582ada3111e07239eecc882cec38
Jakob <jakob.willforss@hotmail.com>
2020-01-07 13:11:28 +0100

    Refactor reactive values into its own script

--- modules/module_setup.R
+++ modules/module_setup.R
@@ -190,39 +190,25 @@ module_setup_server <- function(input, output, session) {
         updateSelectInput(session, "shown_fields", choices = comb_data_cols, selected=start_select)
     })
     
+    observeEvent({
+        input$trunc_length
+        input$round_digits
+        input$shown_fields
+    }, {
+        message("Updating table settings")
+        rv$table_settings(list(
+            trunc_length = input$trunc_length,
+            round_digits = input$round_digits,
+            shown_fields = input$shown_fields
+        ))
+    })
+    
     output$dt_data1 <- DT::renderDataTable({
-        # req(rv$filedata_1)
-        # rv$filedata_1() 
-        
+
         req(rv$mapping_obj())
         req(rv$mapping_obj()$get_combined_dataset())
-        
-        shown_data <- rv$mapping_obj()$get_combined_dataset()
-        
-        if (is.null(rv$selected_feature())) {
-            selected_row_nbr <- 1
-        }
-        else {
-            selected_row_nbr <- which(shown_data$comb_id %>% as.character() %in% rv$selected_feature())
-        }
-        
-        # round_digits <- 3
-        # trunc_length <- 20
-        
-        shown_data %>%
-            dplyr::select(input$shown_fields) %>%
-            mutate_if(
-                is.character,
-                ~str_trunc(., input$trunc_length)
-            ) %>%
-            mutate_if(
-                is.numeric,
-                ~round(., input$round_digits)
-            ) %>%
-            DT::datatable(
-                data=., 
-                selection=list(mode='single', selected=c(selected_row_nbr)), 
-                options=list(pageLength=10))
+
+        rv$dt_parsed_data(rv)
     })
         
     output$dt_design1 <- DT::renderDataTable({
@@ -230,88 +216,12 @@ module_setup_server <- function(input, output, session) {
         rv$design_1()
     })
     
-    # output$dt_data2 <- DT::renderDataTable({
-    #     req(rv$filedata_2)
-    #     rv$filedata_2()
-    # })
-    
     output$dt_design2 <- DT::renderDataTable({
         req(rv$design_2)
         rv$design_2()
     })
     
-    load_data <- function(in_file) {
-        infile <- in_file
-        if (is.null(infile)) {
-            return(NULL)
-        }
-        raw_df <- read_tsv(infile$datapath, col_types = cols())
-        colnames(raw_df) <- make.names(colnames(raw_df))
-        raw_df
-    }
-    
-    get_filename <- function(in_file) {
-        infile <- in_file
-        if (is.null(infile)) {
-            return(NULL)
-        }
-        stringi::stri_extract_first(str = infile$name, regex = ".*")
-    }
-    
-    rv <- list()
-    rv$filedata_1 <- reactive(load_data(input$data_file_1))
-    rv$filedata_2 <- reactive(load_data(input$data_file_2))
-    rv$design_1 <- reactive(load_data(input$design_file_1))
-    rv$design_2 <- reactive({
-        if (!input$matched_samples) {
-            load_data(input$design_file_2)
-        }
-        else {
-            load_data(input$design_file_1)
-        }
-    })
-    rv$design_samplecol_1 <- reactive(input$design_sample_col_1)
-    rv$design_samplecol_2 <- reactive(input$design_sample_col_2)
-    rv$design_condcol_1 <- reactive(input$design_cond_col_1)
-    rv$design_condcol_2 <- reactive(input$design_cond_col_2)
-    rv$selected_cols_obj <- reactiveVal(list())
-    rv$filename_1 <- reactive(get_filename(input$data_file_1))
-    rv$filename_2 <- reactive(get_filename(input$data_file_2))
-    rv$mapping_obj <- reactiveVal(NULL)
-    rv$selected_feature <- reactiveVal(NULL)
-    rv$correlations <- reactiveVal(NULL)
-
-    retrieve_data <- function(rv, input_field, ind, data_pat) {
-        if (!is.null(di_new(rv, input_field, 1))) rv[[sprintf("%s_%s", data_pat, di_new(rv, input_field, ind))]]()
-        else NULL
-    }
-    
-    rv$rdf_ref <- function(rv, input_field) retrieve_data(rv, input_field, 1, "filedata")
-    rv$rdf_comp <- function(rv, input_field) retrieve_data(rv, input_field, 2, "filedata")
-    rv$ddf_ref <- function(rv, input_field) retrieve_data(rv, input_field, 1, "design")
-    rv$ddf_comp <- function(rv, input_field) retrieve_data(rv, input_field, 2, "design")
-    rv$rdf_cols_ref <- function(rv, input_field) colnames(retrieve_data(rv, input_field, 1, "filedata"))
-    rv$rdf_cols_comp <- function(rv, input_field) colnames(retrieve_data(rv, input_field, 2, "filedata"))
-    rv$ddf_cols_ref <- function(rv, input_field) colnames(retrieve_data(rv, input_field, 1, "design"))
-    rv$ddf_cols_comp <- function(rv, input_field) colnames(retrieve_data(rv, input_field, 2, "design"))
-
-    rv$ddf_condcol_ref <- function(rv, input_field) { 
-        rv[[sprintf("design_condcol_%s", di_new(rv, input_field, 1))]]()
-    } 
-    rv$ddf_condcol_comp <- function(rv, input_field) { 
-        rv[[sprintf("design_condcol_%s", di_new(rv, input_field, 2))]]() 
-    } 
-    
-    rv$ddf_samplecol_ref <- function(rv, input_field) {
-        rv[[sprintf("design_samplecol_%s", di_new(rv, input_field, 1))]]()
-    }
-    rv$ddf_samplecol_comp <- function(rv, input_field) {
-        rv[[sprintf("design_samplecol_%s", di_new(rv, input_field, 2))]]()
-    }
-    
-    rv$samples <- function(rv, input_field, prefix="") { 
-        paste(prefix, rv$selected_cols_obj()[[input_field]]$samples, sep="") 
-    }
+    rv <- setup_reactive_values_obj(input)
     
     statcols <- function(rv, data_field, contrast_field, prefix_index=NULL) { 
         
